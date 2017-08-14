3dff3b3048b7dfdb8b52a28620606b4b4327b0bb
John Murphy <chiefsmurph@gmail.com>
2017-08-13 21:37:31 -0700

    github pages routes

--- views/index.js
+++ views/index.js
@@ -3,10 +3,16 @@ import Home from './home';
 import Query from './query';
 
 export default [
+    // local
     ['/', Home],
     ['/home', Home],
     ['/query', Query],
-    ['*', Query]
+    // github pages
+    ['/wiki-hops/', Home],
+    ['/wiki-hops/home', Home],
+    ['/wiki-hops/query', Query],
+    // 404
+    ['*', Home]
     // ['*', (state, actions) => {
     //   return (
     //     <b>{location.pathname}</b>
